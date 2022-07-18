a77ffa88cc5baf18daff83da38c9fbe562a8dee5
estefaniazocar <estefanyazocar3@gmail.com>
2022-07-17 19:51:28 -0500

    subir proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+//console.log(validator);
+//Lo que esta arriba es del index.js original de laboratoria
+
+document.getElementById("btnAll").addEventListener("click", function () {
+  let cardNumber = document.getElementById("numberCard").value;
+  let result = document.getElementById("result");
+  let btnCard = document.getElementById("Card");
+  let btnRetVal = document.getElementById("bttRetVal");
+  let textR = "";
+  /* Validar*/
+
+  let newNumber = validator.isValid(cardNumber);
+  if (newNumber === true) {
+    textR = "es valida!";
+  } else {
+    textR = "no es valido! Por favor comunicate con tu proveedor o entiendad bancaria.";
+    document.getElementById("result").style.color = "red";
+  }
+
+  /* Mascara */
+
+  let nMaskify = validator.maskify(cardNumber);
+
+  result.innerText = "¡Tu tarjeta número " + nMaskify + " " + textR;
+  btnCard.style.display = "none";
+  btnRetVal.style.display = "block";
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,59 @@
 const validator = {
-  // ...
-};
+  isValid: (creditCardNumber) => {
+    const reverseCredit = creditCardNumber.toString().split("").reverse();
+    const total = [];
 
+    //i = es la posición / e = es el valor
+    reverseCredit.forEach((e, i) => {
+      if (i % 2 !== 0) {
+        // i = separa las posicies pares
+        let numPair = e * 2;
+        console.log(reverseCredit)
+        console.log(numPair)
+        // e = multiplica por 2 los valores de las posiciones i
+        if (numPair >= 10) {
+          total.push(
+            numPair
+              .toString()
+              .split("")
+              .reduce((a, b) => parseInt(a) + parseInt(b))
+            // .toString = lo convierte en una cadena
+            // .split = lo separa por caracter
+            // parseInt (a) + (b) = vuelve números los digitos que separa .reduce
+            // .reduce = Cada caracter => a 1 digito lo separa y lo suma
+          );
+        } else {
+          total.push(numPair);
+          // .push añade un valor a un array
+        }
+      } else {
+        total.push(parseInt(e));
+        // capta los número que estaban en posiciones impares
+      }
+    });
+
+    const totalSum = total.reduce((a, b) => a + b);
+    // toma de inicio a fin el valor total y lo suma todo
+    if (totalSum % 10 === 0) {
+      // si termina en 0 retorna en true, si no, en false
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  maskify: (creditCardNumber) => {
+    let maskify = "";
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i <= creditCardNumber.length - 5) {
+        maskify = maskify + "#";
+      } else {
+        maskify = maskify + creditCardNumber[i];
+      }
+      //result.innerText = "¡Tu tarjeta número " + maskify + " es válida !";
+    }
+
+    return maskify;
+  },
+};
 export default validator;
